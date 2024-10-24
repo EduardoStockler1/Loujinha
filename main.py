e498da581a61c3e70d37983a7d48813bb98c3924
JuanLorenzoHarz <juanlorenzoharz@gmail.com>
2024-10-23 23:11:35 -0300

    Update main.py
    Ainda vou integrar completamente, por hora não funciona

--- main.py
+++ main.py
@@ -25,7 +25,16 @@ class Product:
     def listar_produto(product):
         cursor.execute("SELECT * FROM inventory")
         return cursor.fetchall()
-    
+def checkStock(QuantityInStoke, MininumStoke, StokeInfo):
+    if QuantityInStoke <= MininumStoke:
+        StokeInfo = 1
+        return StokeInfo
+def demandForecast(nextDemand, lastDemand, infoActualItem ,seasonalFactor, periodsNumbers):
+    nextDemand = (lastDemand + (infoActualItem / seasonalFactor))/periodsNumbers
+    return nextDemand
+def sells(product, productStoke, quantityBuyed):
+    return productStoke-quantityBuyed
+
 produto = Product(12, "Galaxy J5", 12.40, "Celular Samsung", "Eletronicos", 40)
 
 produto.cadastrar_produto()

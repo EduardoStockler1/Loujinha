965ad92f6f9317245efa10af17d506081552c903
Eduardo Stockler <stocklerdudu@gmail.com>
2024-10-27 21:44:15 -0300

    removendo stoke.py

--- stockFunctions.py
+++
@@ -1,9 +0,0 @@
-def checkStock(QuantityInStoke, MininumStoke, StokeInfo):
-    if QuantityInStoke <= MininumStoke:
-            StokeInfo = 1
-            return StokeInfo
-def demandForecast(nextDemand, lastDemand, infoActualItem ,seasonalFactor, periodsNumbers):
-    nextDemand = (lastDemand + (infoActualItem / seasonalFactor))/periodsNumbers
-    return nextDemand
-def sells(product, productStoke, quantityBuyed):
-    return productStoke-quantityBuyed
